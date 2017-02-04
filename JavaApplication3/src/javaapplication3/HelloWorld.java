db50d931644b29141861d545420a7457c82faa73
Nifeo <Nifeo@RickCom.fritz.box>
2017-02-05 02:46:58 +1300

    TryThatOneasdf

--- JavaApplication3/src/javaapplication3/HelloWorld.java
+++ JavaApplication3/src/javaapplication3/HelloWorld.java
@@ -12,5 +12,6 @@ package javaapplication3;
 public class HelloWorld {
     public static void main(String args[]){
         System.out.println("helloWorld!!");
+        System.out.println("I change some");
     }
 }
