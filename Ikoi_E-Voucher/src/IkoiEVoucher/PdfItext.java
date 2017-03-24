07a9f4ba47e1d83bbd0b1b8c9d58c23504a4c7ea
Nifeo <beiyongju@gmail.com>
2017-03-24 19:22:22 +1300

    compacity for the moblie devices.

--- Ikoi_E-Voucher/src/IkoiEVoucher/PdfItext.java
+++ Ikoi_E-Voucher/src/IkoiEVoucher/PdfItext.java
@@ -1,4 +1,5 @@
 package IkoiEVoucher;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -7,32 +8,40 @@ package IkoiEVoucher;
 
 import com.itextpdf.forms.PdfAcroForm;
 import com.itextpdf.forms.fields.PdfFormField;
+import com.itextpdf.io.image.ImageData;
+import com.itextpdf.io.image.ImageDataFactory;
+import com.itextpdf.kernel.geom.PageSize;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
+import com.itextpdf.kernel.pdf.extgstate.PdfExtGState;
+import com.itextpdf.layout.Document;
+import com.itextpdf.pdfa.PdfADocument;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Map;
-import sun.tools.jar.Main;
 
 /**
  *
  * @author Rick
  */
 public class PdfItext {
-
-    private final InputStream src;
-    private final OutputStream dest;
     PdfDocument pdfDoc;
     String fileName;
-    
-    public PdfItext(String dest) throws IOException{
-        this.src = Main.class.getResourceAsStream("/src/E_Voucher.pdf");
-        this.dest = new FileOutputStream(new File(System.getProperty("user.dir")+"\\"+ dest));
-        this.pdfDoc = new PdfDocument(new PdfReader(this.src), new PdfWriter(this.dest));
+
+    public PdfItext(String filesrc, String filedest) throws IOException {
+        InputStream src1 = new FileInputStream(new File(System.getProperty("user.dir") + "\\" + filesrc));
+        OutputStream dest1 = new FileOutputStream(new File(System.getProperty("user.dir") + "\\" + filedest));
+        this.pdfDoc = new PdfDocument(new PdfReader(src1), new PdfWriter(dest1));
+    }
+
+    public PdfItext() throws FileNotFoundException, IOException {
     }
 
     public void fillFormField(String formName, String value) throws IOException {
@@ -41,8 +50,31 @@ public class PdfItext {
         fields.get(formName).setValue(value);
         fields.get(formName).setReadOnly(true);
     }
-     
-    public void close(){
+
+    public void imageToPDF(String filedest, String imageSrc) throws FileNotFoundException, IOException {
+        PdfWriter pw = new PdfWriter(filedest);
+        pdfDoc = new PdfDocument(pw);
+        PageSize ps = new PageSize(PageSize.A4);
+        Document dc = new Document(pdfDoc, ps);
+        PdfCanvas canvas = new PdfCanvas(pdfDoc.addNewPage());
+        ImageData image = ImageDataFactory.create(imageSrc);
+        canvas.saveState();
+        PdfExtGState state = new PdfExtGState();
+        canvas.setExtGState(state);
+        canvas.addImage(image,0,0,ps.getWidth(),false);
+        canvas.restoreState();
+        dc.close();
+    }
+
+    public PdfDocument getPdfDoc() {
+        return pdfDoc;
+    }
+    
+    public void close() {
         pdfDoc.close();
+    }
+
+    public String getFileName() {
+        return fileName;
     }
 }
